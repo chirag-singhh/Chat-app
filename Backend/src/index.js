0c9f396c157425101bb989c04fbf520e1aaaf67c
chirag-singhh <cs5764532@gmail.com>
2025-01-23 10:35:01 +0530

    ..

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -27,12 +27,12 @@ const __dirname = path.resolve();
 app.use("/auth",authRoutes)
 app.use("/message",messageRoutes)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 
   
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
   }
 
